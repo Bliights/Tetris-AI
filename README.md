4199dd919b7e845284af99edadfd4d66b4f908b6
MilkshakesBot <47411315+MilkshakesBot@users.noreply.github.com>
2023-09-27 13:17:09 +0900

    init commit of tetris
    very basic Tetris game. there are a lot of things not in / broken in it so will need revisions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Tetris_ai
+
+# How to run
+
+1. Install pygame
+   `pip install -r requirements.txt`
+2. Run `python tetris.py`
+
+## TODO
+
+- [] Correct size of the board
+- [] Add score
---
+++ tetris.py
@@ -0,0 +1,174 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Tetris game constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+GRID_SIZE = 50
+GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+FPS = 30
+GRAVITY = 1
+
+# Tetris shapes
+SHAPES = [
+    [[1, 1, 1, 1]],
+    [[1, 1], [1, 1]],
+    [[1, 1, 0], [0, 1, 1]],
+    [[0, 1, 1], [1, 1, 0]],
+    [[1, 0, 0], [1, 1, 1]],
+    [[0, 0, 1], [1, 1, 1]],
+    [[1, 1, 1], [0, 0, 1]],
+]
+
+
+class TetrisGame:
+    def __init__(self):
+        self.board = [[0] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
+        self.current_piece = self.generate_piece()
+        self.next_piece = self.generate_piece()
+        self.score = 0
+        self.gravity_timer = FPS
+
+    def generate_piece(self):
+        shape = random.choice(SHAPES)
+        piece = {
+            "shape": shape,
+            "row": 0,
+            "col": GRID_WIDTH // 2 - len(shape[0]) // 2
+        }
+        return piece
+
+    def rotate_piece(self):
+        rotated_shape = list(zip(*reversed(self.current_piece["shape"])))
+        if self.is_valid_move(rotated_shape, self.current_piece["row"], self.current_piece["col"]):
+            self.current_piece["shape"] = rotated_shape
+
+    def move_piece_left(self):
+        if self.is_valid_move(self.current_piece["shape"], self.current_piece["row"], self.current_piece["col"] - 1):
+            self.current_piece["col"] -= 1
+
+    def move_piece_right(self):
+        if self.is_valid_move(self.current_piece["shape"], self.current_piece["row"], self.current_piece["col"] + 1):
+            self.current_piece["col"] += 1
+
+    def move_piece_down(self):
+        if self.is_valid_move(self.current_piece["shape"], self.current_piece["row"] + 1, self.current_piece["col"]):
+            self.current_piece["row"] += 1
+        else:
+            self.place_piece()
+
+    def place_piece(self):
+        shape = self.current_piece["shape"]
+        row = self.current_piece["row"]
+        col = self.current_piece["col"]
+
+        for i in range(len(shape)):
+            for j in range(len(shape[0])):
+                if shape[i][j] == 1:
+                    self.board[row + i][col + j] = 1
+
+        self.clear_lines()
+        self.current_piece = self.next_piece
+        self.next_piece = self.generate_piece()
+
+        if not self.is_valid_move(self.current_piece["shape"], self.current_piece["row"], self.current_piece["col"]):
+            self.reset()
+
+    def clear_lines(self):
+        full_lines = []
+        for i in range(GRID_HEIGHT):
+            if all(self.board[i]):
+                full_lines.append(i)
+
+        for line in full_lines:
+            self.board.pop(line)
+            self.board.insert(0, [0] * GRID_WIDTH)
+
+        self.score += len(full_lines)
+
+    def is_valid_move(self, shape, row, col):
+        for i in range(len(shape)):
+            for j in range(len(shape[0])):
+                if (
+                        (shape[i][j] == 1)
+                        and (
+                        (row + i >= GRID_HEIGHT)
+                        or (col + j < 0 or col + j >= GRID_WIDTH)
+                        or (self.board[row + i][col + j])
+                )
+                ):
+                    return False
+        return True
+
+    def reset(self):
+        self.board = [[0] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
+        self.current_piece = self.generate_piece()
+        self.next_piece = self.generate_piece()
+        self.score = 0
+
+
+# Initialize game display
+display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Tetris Game")
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (33, 102, 196)
+GREEN = (92, 184, 92)
+RED = (217, 83, 79)
+
+# Initialize Game
+game = TetrisGame()
+
+clock = pygame.time.Clock()
+is_running = True
+
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_piece_left()
+            elif event.key == pygame.K_RIGHT:
+                game.move_piece_right()
+            elif event.key == pygame.K_DOWN:
+                game.move_piece_down()
+            elif event.key == pygame.K_UP:
+                game.rotate_piece()
+
+    # Update game logic
+    game.gravity_timer -= 1
+    if game.gravity_timer == 0:
+        game.move_piece_down()
+        game.gravity_timer = FPS // GRAVITY
+
+    # Update display
+    display.fill(BLACK)
+
+    # Draw board
+    for i in range(GRID_HEIGHT):
+        for j in range(GRID_WIDTH):
+            if game.board[i][j] == 1:
+                pygame.draw.rect(display, GREEN, (j * GRID_SIZE, i * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(display, WHITE, (j * GRID_SIZE, i * GRID_SIZE, GRID_SIZE, GRID_SIZE), 1)
+
+    # Draw current piece
+    shape = game.current_piece["shape"]
+    row = game.current_piece["row"]
+    col = game.current_piece["col"]
+    for i in range(len(shape)):
+        for j in range(len(shape[0])):
+            if shape[i][j] == 1:
+                pygame.draw.rect(display, BLUE, ((col + j) * GRID_SIZE, (row + i) * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+# Quit the game
+pygame.quit()
